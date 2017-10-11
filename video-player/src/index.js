91c0f43d454d42be61cb3faadde11234e6f1f4d2
Neota Moe <neota.moe@gmail.com>
2017-10-10 20:10:17 -0500

    through section 2 complete, video app without redux done

--- video-player/src/index.js
+++ video-player/src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import key from './api_key';
@@ -17,8 +18,11 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
+    this.videoSearch('surfboards');
+  }
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -26,10 +30,13 @@ class App extends Component {
       // this.setState({videos: videos}) --> can use shorthand above when key and value are same
     });
   }
+
   render() {
+    const videoSearch = _.debounce ((term) => {this.videoSearch(term)}, 300);
+
     return(
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
