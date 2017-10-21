d2c5beb6de085e3ab80473573e50d95e1ad3032f
Neota Moe <neota.moe@gmail.com>
2017-10-21 11:01:19 -0500

    initial request and setup done

--- blog-app/src/components/posts_index.js
+++ blog-app/src/components/posts_index.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions';
 
 class PostsIndex extends Component {
+
+  componentDidMount(){
+    this.props.fetchPosts();
+  }
+
   render(){
     return (
       <div>
@@ -10,4 +17,6 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex;
+// this is same as matchDispatchToProps in separate function as done in previous apps
+// export default connect(null, {fetchPosts: fetchPosts})(PostsIndex); --> below is shortened with ES6 syntax
+export default connect(null, {fetchPosts})(PostsIndex);
